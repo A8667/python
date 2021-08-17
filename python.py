eaeada2bd19703ea3490a327485ce47572c0723d
A8667 <89060095+A8667@users.noreply.github.com>
2021-08-17 10:59:57 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = 5
+y = 7
+
+z1 = x + y
+
+z2 = x - y
+
+z3 = x*y
+
+z4 = x/y
+
+z5 = x + 2*y
+
+z6 = 3*x + 2*y
+
+z7 = x**2 * y**2
+
+import matplotlib.pyplot as plt
+
+v=[1,2,3,4,4]
+
+plt.plot(v)
+
+l1=[1,True,"sam",2.44]
+
+l2=list(range(10))
+
+print(l2)
+
+print(l1[-2])
+
+l2[2:6]
